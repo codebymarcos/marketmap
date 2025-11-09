27e1ee24801b642c535acae1211ffcea28352f07
marcosgomes <marcosgabrielgomes105@gmail.com>
2025-11-09 13:25:50 -0500

    main atualizado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cli.title import print_banner
 from InquirerPy import inquirer
 from colorama import init, Fore, Style
-from functs.gerardb import generate_db
+from core.__gruposnowhats__.run import run_all
 from functs.cleardb import clear_data
 
 # Inicializar colorama
@@ -14,13 +14,13 @@ def main():
 	# menu inicial
 	answer = inquirer.select(
 		message="Escolha uma opção",
-		choices=["criar base de dados", "limpar base de dados", "Opção 3"],
-		default="criar base de dados"
+		choices=["criar base de dados (ambos os sites)", "limpar base de dados", "Opção 3"],
+		default="criar base de dados (ambos os sites)"
 	).execute()
 
-	if answer == "criar base de dados":
-		print(Fore.GREEN + "Você escolheu criar base de dados" + Style.RESET_ALL)
-		generate_db()
+	if answer == "criar base de dados (ambos os sites)":
+		print(Fore.GREEN + "Você escolheu criar base de dados (coleta máxima de invites)" + Style.RESET_ALL)
+		run_all()
 	elif answer == "limpar base de dados":
 		print(Fore.BLUE + "Você escolheu limpar base de dados" + Style.RESET_ALL)
 		clear_data()
